c9b4efd5d21f5f83435f77f21cced6351bd30ba4
JGedlu <gedlujay@Gmail.com>
2025-09-23 22:35:52 -0400

    Shapes the data for keras

--- MLModel.py
+++ MLModel.py
@@ -6,3 +6,7 @@ import matplotlib.pyplot as plt
 # Generates the data (in -> cm)
 inches = np.linspace(0, 100, 100)           # Creates 100 numbers (0 - 100)
 cm = inches * 2.54                                          # Converts inches to centimeters
+
+# Shapes data for Keras (samples, features)
+inches = inches.reshape(-1, 1)
+cm = cm.reshape(-1, 1)
