e5d1b42d7909fb3824023ad0304e6343153527f8
JGedlu <gedlujay@Gmail.com>
2025-09-23 22:34:37 -0400

    Added data generation functionality

--- MLModel.py
+++ MLModel.py
@@ -2,3 +2,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 import matplotlib.pyplot as plt
+
+# Generates the data (in -> cm)
+inches = np.linspace(0, 100, 100)           # Creates 100 numbers (0 - 100)
+cm = inches * 2.54                                          # Converts inches to centimeters
